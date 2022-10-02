b0d662dd187bc08769e69e162dbc9ebe4d379587
Mathieu Boucher <mathieu.boucher@epsi.fr>
2022-10-02 11:12:22 +0200

    Add Home, Map and Sidebar components

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export default class Home extends React.Component<any, any> {
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export default class Sidebar extends React.Component<any, any> {
+    render() {
+        return (
+            <View></View>
+        );
+    }
+}
